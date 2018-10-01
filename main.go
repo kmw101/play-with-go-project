8a219016e0439539706552082852c7e017f5b548
kmw101 <30301533+kmw101@users.noreply.github.com>
2018-10-01 16:25:21 +0100

    Added Crypro Struct

--- main.go
+++ main.go
@@ -3,10 +3,29 @@ package main
 
 import "fmt"
 
+// Create a simple token definition
+type cryptoCoin struct{
+	coinName, tokenType  string
+	numberOfCoins int64
+}
+
 func main() {
 	fmt.Printf("Hiya")
 	// Loop and print counter
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+	// Create Fake Coin
+	fakeCoin := cryptoCoin{
+		coinName = "Fako"
+		tokenType = "ERC20"
+		numberOfCoins = 1000000
+	}
+	// Create Security Token
+	securityToken := cryptoCoin{
+		coinName = "Asset Backed Token"
+		tokenType = "EOS"
+		numberOfCoins = 25000000
+	}
+
 }
