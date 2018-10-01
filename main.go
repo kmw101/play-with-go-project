f228a3e730881f077fb93dbbabeee986616fe762
kmw101 <30301533+kmw101@users.noreply.github.com>
2018-10-01 16:31:27 +0100

    printing crypto fields

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ package main
 import "fmt"
 
 // Create a simple token definition
-type cryptoCoin struct{
-	coinName, tokenType  string
-	numberOfCoins int64
+type cryptoCoin struct {
+	coinName, tokenType string
+	numberOfCoins       int64
 }
 
 func main() {
@@ -17,15 +17,15 @@ func main() {
 	}
 	// Create Fake Coin
 	fakeCoin := cryptoCoin{
-		coinName = "Fako"
-		tokenType = "ERC20"
-		numberOfCoins = 1000000
-	}
+		coinName:      "Fako",
+		tokenType:     "ERC20",
+		numberOfCoins: 1000000}
 	// Create Security Token
 	securityToken := cryptoCoin{
-		coinName = "Asset Backed Token"
-		tokenType = "EOS"
-		numberOfCoins = 25000000
-	}
-
+		coinName:      "Asset Backed Token",
+		tokenType:     "EOS",
+		numberOfCoins: 25000000}
+	// Print some of the field names.
+	fmt.Println(fakeCoin.coinName)
+	fmt.Println(securityToken.tokenType)
 }
